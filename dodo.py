9a1fea4efa5909bb79305c5dab5d7f353b32fb5c
Eduardo Schettino <schettino72@gmail.com>
2013-10-07 10:09:02 +0800

    added dodo.py

---
+++ dodo.py
@@ -0,0 +1,38 @@
+import os
+import glob
+
+
+DOIT_CONFIG = {'default_tasks': ['checker', 'test']}
+
+
+PY_FILES = glob.glob('*.py')
+
+def task_checker():
+    """run pyflakes on all project files"""
+
+    def add_pyflakes_builtins():
+        os.environ['PYFLAKES_BUILTINS'] = 'unicode'
+    yield {
+        'basename': '_pyflakes_builtins',
+        'actions': [add_pyflakes_builtins]
+        }
+
+    for module in PY_FILES:
+        yield {'actions': ["pyflakes %(dependencies)s"],
+               'name':module,
+               'file_dep':(module,),
+               'setup':['_pyflakes_builtins'],
+               'title': (lambda task: task.name)
+               }
+
+def task_test():
+    return {
+        'actions': ['py.test'],
+        'file_dep': PY_FILES,
+        }
+
+def task_coverage():
+    return {
+        'actions': ['py.test --cov pyterm.py --cov test_pyterm.py --cov-report term-missing'],
+        'verbosity': 2,
+        }
