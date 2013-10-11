d797544eff19b5350cdaea863f080f0a221fb608
Eduardo Schettino <schettino72@gmail.com>
2013-10-11 14:16:15 +0800

    generate docs, added README and CHANGES files.

--- ddemo.py
+++ ddemo.py
@@ -1,28 +1,80 @@
+"""Fake output as if code was typed in an interactive interpreter
+
+This is used to generate docs
+"""
+
 import ast
+import sys
 
-source = open('sample_tutorial.py').read()
+from pyterm import Term
 
+
+# copy from doit.action
+class Writer(object):
+    """write to many streams"""
+    def __init__(self, *writers):
+        """@param writers - file stream like objects"""
+        self.writers = []
+        self._isatty = True
+        for writer in writers:
+            self.add_writer(writer)
+
+    def add_writer(self, stream, isatty=None):
+        """adds a stream to the list of writers
+        @param isatty: (bool) if specified overwrites real isatty from stream
+        """
+        self.writers.append(stream)
+        isatty = stream.isatty() if (isatty is None) else isatty
+        self._isatty = self._isatty and isatty
+
+    def write(self, text):
+        """write 'text' to all streams"""
+        for stream in self.writers:
+            stream.write(text)
+
+    def flush(self):
+        """flush all streams"""
+        for stream in self.writers:
+            stream.flush()
+
+    def isatty(self):
+        return self._isatty
+
+    def fileno(self):
+        return 0 #return whatever
+
+# fake our sys.stdout to be always considered a tty
+new_out = Writer()
+new_out.add_writer(sys.stdout, isatty=True)
+sys.stdout = new_out
+
+
+
+
+source = open('tutorial.py').read()
+
+term = Term()
 block = []
 for line in source.splitlines():
     if block and (not line or line[0] != ' '):
         # previous block finished
-        print('...')
+        term.BOLD('...\n')
         exec(compile('\n'.join(block), '<string>', 'exec'))
         block = []
 
     if not line:
-        print('>>>')
+        term.BOLD('>>>\n')
     else:
         if line[0] != ' ':
             # single line or block start
             try:
-                print('''>>> {}'''.format(line))
+                term.BOLD('''>>> {}\n'''.format(line))
                 ast.parse(line)
                 exec(compile(line, '<string>', 'exec'))
             except:
                 block.append(line)
         else:
-            print('''... {}'''.format(line))
+            term.BOLD('''... {}\n'''.format(line))
             block.append(line)
 
 if block:
--- dodo.py
+++ dodo.py
@@ -36,3 +36,11 @@ def task_coverage():
         'actions': ['py.test --cov pyterm.py --cov test_pyterm.py --cov-report term-missing'],
         'verbosity': 2,
         }
+
+
+def task_docs():
+    return {
+        'actions': ['python ddemo.py | ansi2html > tutorial.html'],
+        'file_dep': ['pyterm.py', 'tutorial.py', 'ddemo.py',],
+        'targets': ['tutorial.html'],
+        }
